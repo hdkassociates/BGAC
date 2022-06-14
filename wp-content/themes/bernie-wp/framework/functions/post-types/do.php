c1c5d29d1e94b5136dffff92ec735c9e25da7853
Greig Cooke <greigcooke75@gmail.com>
2022-06-14 17:19:21 +0100

    update do CPT to visit

--- wp-content/themes/bernie-wp/framework/functions/post-types/do.php
+++ wp-content/themes/bernie-wp/framework/functions/post-types/do.php
@@ -3,23 +3,23 @@
 function create_dos_post_type() {
 
     $labels = array(
-        'name'                => _x( 'Dos', 'Post Type General Name', 'horiondigital' ),
-        'singular_name'       => _x( 'Do', 'Post Type Singular Name', 'horiondigital' ),
-        'menu_name'           => __( 'Do', 'horiondigital' ),
-        'parent_item_colon'   => __( 'Parent Do:', 'horiondigital' ),
-        'all_items'           => __( 'All Dos', 'horiondigital' ),
-        'view_item'           => __( 'View Do', 'horiondigital' ),
-        'add_new_item'        => __( 'Add new Do', 'horiondigital' ),
+        'name'                => _x( 'Visit', 'Post Type General Name', 'horiondigital' ),
+        'singular_name'       => _x( 'Visit', 'Post Type Singular Name', 'horiondigital' ),
+        'menu_name'           => __( 'Visit', 'horiondigital' ),
+        'parent_item_colon'   => __( 'Parent Visit:', 'horiondigital' ),
+        'all_items'           => __( 'All Visit posts', 'horiondigital' ),
+        'view_item'           => __( 'View Visit', 'horiondigital' ),
+        'add_new_item'        => __( 'Add new Visit', 'horiondigital' ),
         'add_new'             => __( 'Add new', 'horiondigital' ),
-        'edit_item'           => __( 'Edit Do', 'horiondigital' ),
-        'update_item'         => __( 'Update Do', 'horiondigital' ),
-        'search_items'        => __( 'Search Do', 'horiondigital' ),
+        'edit_item'           => __( 'Edit Visit', 'horiondigital' ),
+        'update_item'         => __( 'Update Visit', 'horiondigital' ),
+        'search_items'        => __( 'Search Visit', 'horiondigital' ),
         'not_found'           => __( 'Not found', 'horiondigital' ),
         'not_found_in_trash'  => __( 'Not found in Trash', 'horiondigital' ),
     );
     $args = array(
-        'label'               => __( 'Do', 'horiondigital' ),
-        'description'         => __( 'Do Post Type', 'horiondigital' ),
+        'label'               => __( 'Visit', 'horiondigital' ),
+        'description'         => __( 'Visit Post Type', 'horiondigital' ),
         'labels'              => $labels,
         'supports'            => array( 'title', 'thumbnail', 'editor' ),
         'taxonomies'          => array(),
@@ -35,6 +35,7 @@ function create_dos_post_type() {
         'has_archive'         => false,
         'exclude_from_search' => false,
         'publicly_queryable'  => true,
+        'rewrite' => array('slug' => 'visit','with_front' => false),
         'capability_type'     => 'page',
     );
     register_post_type( 'do', $args );
