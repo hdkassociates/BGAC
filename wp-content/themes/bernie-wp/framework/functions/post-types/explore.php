02706447ea1db74ae905205240c2345a209e4464
Greig Cooke <greigcooke75@gmail.com>
2022-06-14 12:09:40 +0100

    Change see CPT to whats-on and explore to about

--- wp-content/themes/bernie-wp/framework/functions/post-types/explore.php
+++ wp-content/themes/bernie-wp/framework/functions/post-types/explore.php
@@ -3,23 +3,23 @@
 function create_explore_post_type() {
 
     $labels = array(
-        'name'                => _x( 'Explore', 'Post Type General Name', 'horiondigital' ),
-        'singular_name'       => _x( 'Explore', 'Post Type Singular Name', 'horiondigital' ),
-        'menu_name'           => __( 'Explore', 'horiondigital' ),
-        'parent_item_colon'   => __( 'Parent Explore:', 'horiondigital' ),
-        'all_items'           => __( 'All Explores', 'horiondigital' ),
-        'view_item'           => __( 'View Explore', 'horiondigital' ),
-        'add_new_item'        => __( 'Add new Explore', 'horiondigital' ),
+        'name'                => _x( 'About', 'Post Type General Name', 'horiondigital' ),
+        'singular_name'       => _x( 'About', 'Post Type Singular Name', 'horiondigital' ),
+        'menu_name'           => __( 'About', 'horiondigital' ),
+        'parent_item_colon'   => __( 'Parent About:', 'horiondigital' ),
+        'all_items'           => __( 'All About Posts', 'horiondigital' ),
+        'view_item'           => __( 'View About', 'horiondigital' ),
+        'add_new_item'        => __( 'Add new About', 'horiondigital' ),
         'add_new'             => __( 'Add new', 'horiondigital' ),
-        'edit_item'           => __( 'Edit Explore', 'horiondigital' ),
-        'update_item'         => __( 'Update Explore', 'horiondigital' ),
-        'search_items'        => __( 'Search Explore', 'horiondigital' ),
+        'edit_item'           => __( 'Edit About', 'horiondigital' ),
+        'update_item'         => __( 'Update About', 'horiondigital' ),
+        'search_items'        => __( 'Search About', 'horiondigital' ),
         'not_found'           => __( 'Not found', 'horiondigital' ),
         'not_found_in_trash'  => __( 'Not found in Trash', 'horiondigital' ),
     );
     $args = array(
-        'label'               => __( 'Explore', 'horiondigital' ),
-        'description'         => __( 'Explore Post Type', 'horiondigital' ),
+        'label'               => __( 'About', 'horiondigital' ),
+        'description'         => __( 'About Post Type', 'horiondigital' ),
         'labels'              => $labels,
         'supports'            => array( 'title', 'thumbnail', 'editor' ),
         'taxonomies'          => array(),
@@ -35,6 +35,7 @@ function create_explore_post_type() {
         'has_archive'         => false,
         'exclude_from_search' => false,
         'publicly_queryable'  => true,
+        'rewrite' => array('slug' => 'about','with_front' => false),
         'capability_type'     => 'page',
     );
     register_post_type( 'explore', $args );
--- wp-content/themes/bernie-wp/framework/functions/post-types/see.php
+++ wp-content/themes/bernie-wp/framework/functions/post-types/see.php
@@ -3,23 +3,23 @@
 function create_see_post_type() {
 
     $labels = array(
-        'name'                => _x( 'See', 'Post Type General Name', 'horiondigital' ),
-        'singular_name'       => _x( 'See', 'Post Type Singular Name', 'horiondigital' ),
-        'menu_name'           => __( 'See', 'horiondigital' ),
-        'parent_item_colon'   => __( 'Parent See:', 'horiondigital' ),
-        'all_items'           => __( 'All See', 'horiondigital' ),
-        'view_item'           => __( 'View See', 'horiondigital' ),
-        'add_new_item'        => __( 'Add new See', 'horiondigital' ),
+        'name'                => _x( 'What\'s on', 'Post Type General Name', 'horiondigital' ),
+        'singular_name'       => _x( 'What\'s on', 'Post Type Singular Name', 'horiondigital' ),
+        'menu_name'           => __( 'What\'s on', 'horiondigital' ),
+        'parent_item_colon'   => __( 'Parent What\'s on:', 'horiondigital' ),
+        'all_items'           => __( 'All What\'s on', 'horiondigital' ),
+        'view_item'           => __( 'View What\'s on', 'horiondigital' ),
+        'add_new_item'        => __( 'Add new What\'s on', 'horiondigital' ),
         'add_new'             => __( 'Add new', 'horiondigital' ),
-        'edit_item'           => __( 'Edit See', 'horiondigital' ),
-        'update_item'         => __( 'Update See', 'horiondigital' ),
-        'search_items'        => __( 'Search See', 'horiondigital' ),
+        'edit_item'           => __( 'Edit What\'s on', 'horiondigital' ),
+        'update_item'         => __( 'Update What\'s on', 'horiondigital' ),
+        'search_items'        => __( 'Search What\'s on', 'horiondigital' ),
         'not_found'           => __( 'Not found', 'horiondigital' ),
         'not_found_in_trash'  => __( 'Not found in Trash', 'horiondigital' ),
     );
     $args = array(
-        'label'               => __( 'See', 'horiondigital' ),
-        'description'         => __( 'See Post Type', 'horiondigital' ),
+        'label'               => __( 'What\'s on', 'horiondigital' ),
+        'description'         => __( 'What\'s on Post Type', 'horiondigital' ),
         'labels'              => $labels,
         'supports'            => array( 'title', 'thumbnail', 'editor' ),
         'taxonomies'          => array(),
@@ -35,6 +35,7 @@ function create_see_post_type() {
         'has_archive'         => false,
         'exclude_from_search' => false,
         'publicly_queryable'  => true,
+        'rewrite' => array('slug' => 'whats-on','with_front' => false),
         'capability_type'     => 'page',
     );
     register_post_type( 'see', $args );
